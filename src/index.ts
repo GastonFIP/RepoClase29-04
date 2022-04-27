f23f7b54232ba358e37f587384ef94148466924c
GastonFIP <104107514+GastonFIP@users.noreply.github.com>
2022-04-27 19:08:25 -0300

    nueva linea Se me cerro y agregue cosas

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,5 @@ console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
 console.log("este repo es de la clase del viernes");
+
+console.log("se me cerro y lo volvi a abrir");
