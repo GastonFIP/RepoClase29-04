e62bffb72e7b9955478d4ccba0a8316dda5a0bdc
GastonFIP <104107514+GastonFIP@users.noreply.github.com>
2022-04-27 19:04:01 -0300

    mas lineas Sumamos varias lineas de codigo, que nos dan un mensaje en la consola

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
+console.log("este repo es de la clase del viernes");
